3e3fb431b2efb649ba2bd12c7722cdbd4d4d1223
MartinHuangR <83437646+MartinHuangR@users.noreply.github.com>
2025-01-28 11:02:45 +1100

    update index

--- Artificial-Simulations.R
+++ Artificial-Simulations.R
@@ -42,69 +42,68 @@ S1.1hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true,
 
 #---#---#---#---#---#---#---#---#---#---#---#---#---#---
 # Simulation 2: SNR = 10
-n = 200; p = 200; active = 20; repeats = 1000;   snr = 10
+n = 100; p = 500; active = 20; repeats = 1000; snr = 10
 true = c(rep(1, active), rep(0,p - active ))
 d =  gendata(n = n, p = p, active = active)
 S2.10hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 2: SNR = 5
-n = 200; p = 200; active = 20; repeats = 1000;  snr = 5
-true = c(rep(1, active), rep(0,p - active))
+n = 100; p = 500; active = 20; repeats = 1000; snr = 5
+true = c(rep(1, active), rep(0,p - active ))
 S2.5hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 2: SNR = 3
-n = 200; p = 200; active = 20; repeats = 1000;  snr = 3
+n = 100; p = 500; active = 20; repeats = 1000; snr = 3
 true = c(rep(1, active), rep(0,p - active ))
 S2.3hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 2: SNR = 1
-n = 200; p = 200; active = 20; repeats = 1000; snr = 1
+n = 100; p = 500; active = 20; repeats = 1000; snr = 1
 true = c(rep(1, active), rep(0,p - active ))
 S2.1hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 
 #---#---#---#---#---#---#---#---#---#---#---#---#---#---
 # Simulation 3: SNR = 10
-n = 500; p = 100; active = 20; repeats = 1000; snr = 10
+n = 200; p = 200; active = 20; repeats = 1000;   snr = 10
 true = c(rep(1, active), rep(0,p - active ))
 d =  gendata(n = n, p = p, active = active)
 S3.10hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 3: SNR = 5
-n = 500; p = 100; active = 20; repeats = 1000; snr = 5
-true = c(rep(1, active), rep(0,p - active ))
+n = 200; p = 200; active = 20; repeats = 1000;  snr = 5
+true = c(rep(1, active), rep(0,p - active))
 S3.5hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 3: SNR = 3
-n = 500; p = 100; active = 20; repeats = 1000; snr = 3
+n = 200; p = 200; active = 20; repeats = 1000;  snr = 3
 true = c(rep(1, active), rep(0,p - active ))
 S3.3hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 3: SNR = 1
-n = 500; p = 100; active = 20; repeats = 1000; snr = 1
+n = 200; p = 200; active = 20; repeats = 1000; snr = 1
 true = c(rep(1, active), rep(0,p - active ))
 S3.1hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
+
 #---#---#---#---#---#---#---#---#---#---#---#---#---#---
 # Simulation 4: SNR = 10
-n = 100; p = 500; active = 10; repeats = 1000; snr = 10
+n = 500; p = 100; active = 20; repeats = 1000; snr = 10
 true = c(rep(1, active), rep(0,p - active ))
 d =  gendata(n = n, p = p, active = active)
 S4.10hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 4: SNR = 5
-n = 100; p = 500; active = 10; repeats = 1000; snr = 5
+n = 500; p = 100; active = 20; repeats = 1000; snr = 5
 true = c(rep(1, active), rep(0,p - active ))
 S4.5hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 4: SNR = 3
-n = 100; p = 500; active = 10; repeats = 1000; snr = 3
+n = 500; p = 100; active = 20; repeats = 1000; snr = 3
 true = c(rep(1, active), rep(0,p - active ))
 S4.3hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
 
 # Simulation 4: SNR = 1
-n = 100; p = 500; active = 10; repeats = 1000; snr = 1
+n = 500; p = 100; active = 20; repeats = 1000; snr = 1
 true = c(rep(1, active), rep(0,p - active ))
 S4.1hard = replicate(repeats, simulationATS(X = d$X, beta = d$beta, true = true, p = p, snr = snr))
-
-
