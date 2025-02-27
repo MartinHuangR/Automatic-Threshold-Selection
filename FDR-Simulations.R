1ccb377838a5757c45881ec43f38d396cf3107e9
MartinHuangR <83437646+MartinHuangR@users.noreply.github.com>
2025-02-28 09:41:04 +1100

    Added FDR

--- FDR-Simulations.R
+++ FDR-Simulations.R
@@ -9,10 +9,10 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 source("Functions.R")
 
-# load("Data/FDR.I.RData")
-# load("Data/FDR.II.RData")
-# load("Data/FDR.III.RData")
-# load("Data/FDR.IV.RData")
+load("Data/FDR.I.RData")
+load("Data/FDR.II.RData")
+load("Data/FDR.III.RData")
+load("Data/FDR.IV.RData")
 
 # Setting 1
 set.seed(1)
@@ -140,15 +140,15 @@ fIVs3e5 = fdr(X = d$X, beta = d$beta, p = p, snr = snr, EV = 5, LOOPS = 1000)
 #--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--
 # Table
 #--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--
-fI = rbind(fIs1e2, fIs3e2, fIs3e5, fIs1e5, fIs3e10, fIs1e10)  |> mutate(Setting = "I")
-fII = rbind(fIIs1e2, fIIs3e2, fIIs3e5, fIIs1e5, fIIs3e10, fIIs1e10) |> mutate(Setting = "II")
-fIII = rbind(fIIIs1e2, fIIIs3e2, fIIIs3e5, fIIIs1e5, fIIIs3e10, fIIIs1e10) |> mutate(Setting = "III")
-fIV = rbind(fIVs1e2, fIVs3e2, fIVs3e5, fIVs1e5, fIVs3e10, fIVs1e10) |> mutate(Setting = "IV")
+fI = rbind(fIs1e2, fIs3e2, fIs1e5, fIs3e5, fIs1e10, fIs3e10)  |> mutate(Setting = "I")
+fII = rbind(fIIs1e2, fIIs3e2, fIIs1e5, fIIs3e5, fIIs1e10, fIIs3e10) |> mutate(Setting = "II")
+fIII = rbind(fIIIs1e2, fIIIs3e2, fIIIs1e5, fIIIs3e5, fIIIs1e10, fIIIs3e10) |> mutate(Setting = "III")
+fIV = rbind(fIVs1e2, fIVs3e2, fIVs1e5, fIVs3e5, fIVs1e10, fIVs3e10) |> mutate(Setting = "IV")
 
 f = rbind(fI, fII, fIII, fIV) |> 
   group_by(Setting, snr, EV) |> 
   summarise(error = 1 - mean(false.selections > EV),
             correct.prop = mean(correct.selections.prop),
             avg.selected = mean(n.selected)) |> 
-  mutate(correct.prop = round(correct.prop, 2))
-
+  mutate(correct.prop = round(correct.prop, 2),
+         avg.selected = round(avg.selected,2))
